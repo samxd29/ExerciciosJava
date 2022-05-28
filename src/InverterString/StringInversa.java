e99ed1a260017e02c923ca327f15bfd3a27e9926
samantha <carvalholuzs@gmail.com>
2022-05-28 20:44:53 -0300

    Exercicios Poo

--- src/InverterString/StringInversa.java
+++ src/InverterString/StringInversa.java
@@ -3,7 +3,11 @@ package InverterString;
 public class StringInversa {
 
     public static String inverterString(String conteudo){
+        // String builder é uma classe para manipular string
         StringBuilder builder = new StringBuilder(conteudo);
+        // tem que passar um parametro para o construtor da classe string builder;
+
+        // criou uma variável que tem um método reverter dentro da classe builder e o to string converter em string
         String invertida = builder.reverse().toString();
         return invertida;
     }
